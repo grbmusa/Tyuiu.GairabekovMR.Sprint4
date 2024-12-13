f7f5e0f226f84f25a5ba041bef14cd84448fbb73
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 04:32:58 +0500

    Task6

---
+++ Tyuiu.GairabekovMR.Sprint4.Task6.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GairabekovMR.Sprint4.Task6.V29.Lib;
+namespace Tyuiu.GairabekovMR.Sprint4.Task6.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] array = { "�����", "����", "�����", "����" };
+            int res = ds.Calculate(array);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint4.Task6.V29/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.GairabekovMR.Sprint4.Task6.V29.Lib;
+namespace Tyuiu.GairabekovMR.Sprint4.Task6.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Гайрабеков | ИИПБ-24-2 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Двумерные массивы (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Гайрабеков | ИИПБ-24-2                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных [Весна, Лето, Осень, Зима] используя        *");
+            Console.WriteLine("* класс Array подсчитайте количество элементов, длина которых больше 4.   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            String[] array = { "Весна", "Лето", "Осень", "Зима" };
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            int count = ds.Calculate(array);
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
