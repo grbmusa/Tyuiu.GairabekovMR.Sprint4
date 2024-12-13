82c3b45bfaf9f59974e1a7a136f920dce6711bf3
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 02:30:48 +0500

    Task2

---
+++ Tyuiu.GairabekovMR.Sprint4.Task2.V19.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.GairabekovMR.Sprint4.Task2.V19.Lib
+{
+    public class DataService : ISprint4Task2V19
+    {
+        public int Calculate(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 1)
+                {
+                    result += array[i];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint4.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GairabekovMR.Sprint4.Task2.V19.Lib;
+
+namespace Tyuiu.GairabekovMR.Sprint4.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new();
+            int[] array = new int[] { 1, 2, 3 };
+            Assert.AreEqual(4, dataService.Calculate(array));
+        }
+    }
+}
