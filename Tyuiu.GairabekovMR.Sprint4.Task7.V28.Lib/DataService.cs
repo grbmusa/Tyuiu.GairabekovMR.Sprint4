b4d655a124dd5fc89655fb661927671efe8f2fe7
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-14 04:46:17 +0500

    Task7

---
+++ Tyuiu.GairabekovMR.Sprint4.Task7.V28.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.GairabekovMR.Sprint4.Task7.V28.Lib
+{
+    public class DataService : ISprint4Task7V28
+    {
+
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] mtrx = new int[n, m];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                }
+            }
+            int r = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mtrx[i, j] % 2 == 0)
+                    {
+                        r = r * mtrx[i, j];
+                    }
+                }
+            }
+            return r;
+        }
+
+
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.GairabekovMR.Sprint4.Task7.V28.Lib;
+namespace Tyuiu.GairabekovMR.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 5;
+            int columns = 3;
+            int[,] mrex = new int[rows, columns];
+            string str = "623351179845632";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4608;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
